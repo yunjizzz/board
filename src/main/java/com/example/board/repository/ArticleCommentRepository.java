1f8c303fe74fbec3ef513384cef4cc7335bd46c3
yunjizzz <60842443+yunjizzz@users.noreply.github.com>
2023-01-29 23:16:56 +0900

    feat : QueryDSL을 이용한 검색 기능 추가
    - QuerydslPredicateExcutor 를 이용한 기본 검색 모두 열기
    - QuerydslBinderCustomizer 를 이용한 특정 조건 검색 추가

--- src/main/java/com/example/board/repository/ArticleCommentRepository.java
+++ src/main/java/com/example/board/repository/ArticleCommentRepository.java
@@ -1,10 +1,31 @@
 package com.example.board.repository;
 
+import com.example.board.domain.Article;
 import com.example.board.domain.ArticleComment;
+import com.example.board.domain.QArticle;
+import com.example.board.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository
+        extends JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>,
+        QuerydslBinderCustomizer<QArticleComment>
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root){
+        bindings.excludeUnlistedProperties(true); // 선택적으로만 검색가능하도록
+        bindings.including(root.content, root.createdAt, root.createdBy);
+        // contains 는 wildcard도 넣어주지만, like는 wildcard를 내가 직접 넣어줘야함.
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
