6abac0ff4c3abc445cbb35744015e25c1ce29980
yunjizzz <60842443+yunjizzz@users.noreply.github.com>
2023-01-29 20:24:51 +0900

    feat : add data.sql
    - 테스트 데이터용 1개

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+-- test date
+insert into  article (title, content,hashtag, created_by, modified_by , modified_at,created_at) values ('제목','내용','#test','yunji','yunji','2021-05-30 23:53:46', '2021-03-10 08:48:50')
