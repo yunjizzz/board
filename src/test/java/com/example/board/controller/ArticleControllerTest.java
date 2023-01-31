4bfbd5c79d8fcf74f5c33a2b69041918d6e3fd8a
yunji <yunjigo92@gmail.com>
2023-01-31 13:10:29 +0900

    feat : articles controller test 추가
    - #15 controller 명세를 위한 test 추가

---
+++ src/test/java/com/example/board/controller/ArticleControllerTest.java
@@ -0,0 +1,79 @@
+package com.example.board.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.awt.*;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * 클래스 설명
+ *
+ * @author : yunji
+ * @version : 1.0.0
+ * @date : 2023/01/31
+ */
+@DisplayName("View controller - 게시글")
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+
+    private final MockMvc mvc;
+    public ArticleControllerTest(@Autowired MockMvc mvc){
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] 게시글 리스트 - 정상 호출")
+    @Test
+    void getArticles() throws Exception {
+        //Given
+
+        // When & Then
+        mvc.perform(MockMvcRequestBuilders.get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles"));
+    }
+
+    @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
+    @Test
+    void getArticleById() throws Exception {
+        //Given
+
+        // When & Then
+        mvc.perform(MockMvcRequestBuilders.get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles"));
+    }
+
+    @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상 호출")
+    @Test
+    void getArticleSearch() throws Exception {
+        //Given
+
+        // When & Then
+        mvc.perform(MockMvcRequestBuilders.get("/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles"));
+    }
+
+
+    @DisplayName("[view][GET] 게시글 해시태그 검색 전용 페이지 - 정상 호출")
+    @Test
+    void getArticleHashTagSearch() throws Exception {
+        //Given
+
+        // When & Then
+        mvc.perform(MockMvcRequestBuilders.get("/articles/search-hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles"));
+    }
+}
